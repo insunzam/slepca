98dca732fab82e457b14c06fc459d70f03b50249
insunzam <62730152+insunzam@users.noreply.github.com>
2022-06-10 10:26:41 -0400

    Act 0906
    Actualizaciones del 9 de junio

--- matricula_agno_comuna_sexo.R
+++ matricula_agno_comuna_sexo.R
@@ -2,6 +2,7 @@ library(ggplot2)
 library(dplyr)
 library(tidyverse)
 library(ggrepel)
+library(summaritools)
 #library(hrbrthemes)
 
 matricula <- readRDS("~/R/projects/slepca/data/matricula16_22.Rdata")
@@ -110,6 +111,58 @@ write_csv(df_area_agn, file = "~/R/projects/slepca/resultados/area_agno.csv")
 
 #Area y Genero
 
+mat_genero_area <- matricula %>% 
+#  filter(AGNO == 2022) %>%
+  group_by(RURAL_RBD) %>%
+  select(RURAL_RBD, MAT_HOM_TOT, MAT_MUJ_TOT) %>%
+  summarize(ALUMNOS = sum(MAT_HOM_TOT), ALUMNAS = sum(MAT_MUJ_TOT))
+
+df_area_genero <- mat_genero_area %>%
+  select(RURAL_RBD, ALUMNAS, ALUMNOS) %>%
+  gather(key= "variable", value = "value", -RURAL_RBD)
+
+df_area_genero$RURAL_RBD <- 
+  replace(df_area_genero$RURAL_RBD, df_area_genero$RURAL_RBD == 0, "URBANO")
+df_area_genero$RURAL_RBD <- 
+  replace(df_area_genero$RURAL_RBD, df_area_genero$RURAL_RBD == 1, "RURAL")
+
+ggplot(df_area_genero, aes(fill=variable, y=value, x=RURAL_RBD)) + 
+  geom_bar(position="dodge", stat="identity") +
+  geom_text(aes(label=value), vjust=1.6, color="white", 
+            position = position_dodge(0.9), size=3.5) +
+  scale_fill_hue(l=40) +
+  theme_minimal() +
+  xlab("Año") + ylab("Matricula") +
+  ggtitle("Matricula Rural/Urbana y genero Costa Araucanía")
+
+write_csv(df_com_genero, file = "~/R/projects/slepca/resultados/genero_comuna.csv")
+
+#Año y Area
+mat_area_agno <- matricula %>% group_by(AGNO) %>%
+  select(AGNO, RURAL_RBD, MAT_TOTAL) %>%
+  summarize(RURAL = sum(MAT_TOTAL[RURAL_RBD == 1]), 
+            URBANO = sum(MAT_TOTAL[RURAL_RBD == 0]))
+
+df_area_agn <- mat_area_agno %>%
+  select(AGNO, RURAL, URBANO) %>%
+  gather(key= "variable", value = "value", -AGNO)
+
+ggplot(df_area_agn, aes(fill=variable, y=value, x=AGNO)) + 
+#  geom_point(aes(size=value)) +
+  geom_line(aes(linetype = variable), size = 1) +
+  geom_label_repel(aes(label = value), nudge_x = 0.35, size = 4) +
+#  geom_bar(position="dodge", stat="identity") +
+#  geom_text(aes(label=value), vjust=1.6, color="white", 
+#            position = position_dodge(0.9), size=3.5) +
+#  scale_size_area(max = 6) +
+  scale_fill_hue(l=40) +
+  theme_minimal() +
+  xlab("Año") + ylab("Matricula") +
+  ggtitle("Matricula por año y ruralidad Costa Araucanía 2016 - 2022")
+
+write_csv(df_area_agn, file = "~/R/projects/slepca/resultados/area_agno.csv")
+
+#Area y Genero
 mat_genero_area <- matricula %>% filter(AGNO == 2022) %>%
   group_by(RURAL_RBD) %>%
   select(RURAL_RBD, MAT_HOM_TOT, MAT_MUJ_TOT) %>%
@@ -128,56 +181,9 @@ ggplot(df_area_genero, aes(fill=variable, y=value, x=RURAL_RBD)) +
   geom_bar(position="dodge", stat="identity") +
   geom_text(aes(label=value), vjust=1.6, color="white", 
             position = position_dodge(0.9), size=3.5) +
-  scale_fill_hue(l=40) +
+  scale_fill_hue(l=20) +
   theme_minimal() +
   xlab("Año") + ylab("Matricula") +
-  ggtitle("Matricula Rural/Urbana y genero Costa Araucanía 2022")
-
-write_csv(df_com_genero, file = "~/R/projects/slepca/resultados/genero_comuna.csv")
-
-#Año y Area
-mat_area_agno <- matricula %>% group_by(AGNO) %>%
-  select(AGNO, RURAL_RBD, MAT_TOTAL) %>%
-  summarize(RURAL = sum(MAT_TOTAL[RURAL_RBD == 1]), 
-            URBANO = sum(MAT_TOTAL[RURAL_RBD == 0]))
-
-df_area_agn <- mat_area_agno %>%
-  select(AGNO, RURAL, URBANO) %>%
-  gather(key= "variable", value = "value", -AGNO)
-
-ggplot(df_area_agn, aes(fill=variable, y=value, x=AGNO)) + 
-  geom_bar(position="dodge", stat="identity") +
-  geom_text(aes(label=value), vjust=1.6, color="white", 
-            position = position_dodge(0.9), size=3.5) +
-  scale_fill_hue(l=40) +
-  theme_minimal() +
-  xlab("Año") + ylab("Matricula") +
-  ggtitle("Matricula por año y ruralidad Costa Araucanía 2016 - 2021")
-
-write_csv(df_area_agn, file = "~/R/projects/slepca/resultados/area_agno.csv")
-
-#Area y Genero
-mat_genero_area <- matricula %>% filter(AGNO == 2021) %>%
-  group_by(RURAL_RBD) %>%
-  select(RURAL_RBD, MAT_HOM_TOT, MAT_MUJ_TOT) %>%
-  summarize(ALUMNOS = sum(MAT_HOM_TOT), ALUMNAS = sum(MAT_MUJ_TOT))
-
-df_area_genero <- mat_genero_area %>%
-  select(RURAL_RBD, ALUMNAS, ALUMNOS) %>%
-  gather(key= "variable", value = "value", -RURAL_RBD)
-
-df_area_genero$RURAL_RBD <- 
-  replace(df_area_genero$RURAL_RBD, df_area_genero$RURAL_RBD == 0, "URBANO")
-df_area_genero$RURAL_RBD <- 
-  replace(df_area_genero$RURAL_RBD, df_area_genero$RURAL_RBD == 1, "RURAL")
-
-ggplot(df_area_genero, aes(fill=variable, y=value, x=RURAL_RBD)) + 
-  geom_bar(position="dodge", stat="identity") +
-  geom_text(aes(label=value), vjust=1.6, color="white", 
-            position = position_dodge(0.9), size=3.5) +
-  scale_fill_hue(l=40) +
-  theme_minimal() +
-  xlab("Año") + ylab("Matricula") +
-  ggtitle("Matricula Rural/Urbana y sexo Costa Araucanía 2021")
+  ggtitle("Matricula Rural/Urbana y sexo Costa Araucanía 2022")
 
 write_csv(df_area_genero, file = "~/R/projects/slepca/resultados/area_genero.csv")
